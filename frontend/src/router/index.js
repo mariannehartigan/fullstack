ff8702740ce751586627117e2dc085b3e9c2c17d
mariannehartigan <mariannehartigan@yahoo.ca>
2022-10-27 14:20:14 -0400

    working frontend and backend with vue

---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from "../views/Index.vue"
+ 
+const routes = [
+  {
+    name: "Index",
+    path: "/",
+    component: Index
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+ 
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+ 
+export default router
