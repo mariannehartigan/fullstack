fb84ad6adf4fbc5ae1228baac802f7b7c75adc7e
mariannehartigan <mariannehartigan@yahoo.ca>
2022-11-01 19:34:22 -0400

    removed product list

--- frontend/src/views/Plan.vue
+++ frontend/src/views/Plan.vue
@@ -1,19 +1,16 @@
 <template>
     <div>
       <In />
-      <ProductList />
     </div>
 </template>
  
 <script>
 // @ is an alias to /src
-import ProductList from '@/components/ProductList.vue'
 import In from '@/components/Plan/In.vue'
 
 export default {
   name: 'Plan',
   components: {
-    ProductList,
     In
   }
 }
